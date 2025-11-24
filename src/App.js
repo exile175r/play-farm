11ae8c77910f5e33cec3087d9c1da60aae1b0978
exile175r <exile175r@gmail.com>
2025-11-24 15:07:35 +0900

    data list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import farmDataJson from './data.json';
 import Home from './components/Home';
 import './App.css';
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,21 +1,63 @@
+import React, { useState } from 'react';
+
 const Home = ({ farmData }) => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
+  // const filteredFarmData = (data, key) => {
+  //   return data.filter(item => Object.keys(item).includes(key) && item[key] !== null);
+  // }
+
+  // const dataWithImg = filteredFarmData(farmData.DATA, "VILLAGE_PHOTO");
+  const { DESCRIPTION: des, DATA: data } = farmData;
+
+  const splitData = [];
+  let newData = [];
+  data.forEach((v, i) => {
+    i += 1;
+    if (i % 21 !== 0) newData.push(v);
+    else {
+      splitData.push(newData);
+      newData = [];
+    }
+    if (i === data.length - 1) splitData.push(newData);
+  });
+  console.log("splitData:", splitData);
+
   return (
     <div>
       <h1>Home</h1>
       <ul className="farm-data-list">
-        {farmData.DATA.map((item, i) => (
+        {splitData[currentPage - 1].map((item, i) => (
           <li key={i}>
-            {Object.keys(item).map((key) => (
-              <p key={key}>
-                <strong>
-                  {Object.keys(farmData.DESCRIPTION).find(k => k === key) ? farmData.DESCRIPTION[key] : key}
-                </strong>
-                <span>{item[key]}</span>
-              </p>
-            ))}
+            <div className="imgBox">
+              <img src={item.VILLAGE_PHOTO ? item.VILLAGE_PHOTO : './images/temp.png'} alt="village" />
+            </div>
+            <div className="infoBox">
+              {Object.keys(item).filter(key => key !== "VILLAGE_PHOTO").map(key => (
+                <p key={key}>
+                  <strong>{Object.keys(des).find(k => k === key) ? des[key] : key}</strong>
+                  {key === "HMPG_ADDR" ?
+                    <a href={item[key]} target="_blank" rel="noopener noreferrer">{item[key]}</a>
+                    :
+                    <span>{item[key]}</span>}
+                </p>
+              ))}
+            </div>
           </li>
         ))}
       </ul>
+      <div className="page-container">
+        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>Prev</button>
+        <ul className="page-list">
+          {splitData.map((_, i) => (
+            <li key={i} className={currentPage === i + 1 ? "active" : ""}>
+              <button onClick={() => setCurrentPage(i + 1)}>{i + 1}</button>
+            </li>
+          ))}
+        </ul>
+        <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === splitData.length}>Next</button>
+      </div>
     </div >
   )
 }
