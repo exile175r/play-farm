a3ab4588bbb20a05ac3d4d2471a865c38bfb708f
Minkyoung <choi90326@gmail.com>
2025-11-27 17:58:45 +0900

    plus

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,30 +1,30 @@
-// src/components/layout/Header.js
 import React from 'react';
 import './Header.css';
+import { getImagePath } from '../../utils/imagePath';
 
 function Header() {
-  return (
-    <header className='pf-header'>
-      <div className='pf-inner'>
-        <h1 className='pf-logo'>
-          <a href='/'>
-            <img src='./images/3kki_logo.png' alt='Play Farm 로고' />
-          </a>
-        </h1>
+   return (
+      <header className="pf-header">
+         <div className="pf-inner">
+            <h1 className="pf-logo">
+               <a href="/">
+                  <img src={getImagePath('/logos/textlogo.png')} alt="logo" />
+               </a>
+            </h1>
 
-        <nav className='pf-nav'>
-          <a href='/'>홈</a>
-          <a href='/'>이벤트</a>
-          <a href='/'>고객지원</a>
-        </nav>
+            <nav className="pf-nav">
+               <a href="/">홈</a>
+               <a href="/">이벤트</a>
+               <a href="/">고객지원</a>
+            </nav>
 
-        <div className='pf-utils'>
-          <button className='pf-login-btn'>login</button>
-          <button className='pf-login-btn'>siginup</button>
-        </div>
-      </div>
-    </header>
-  );
+            <div className="pf-utils">
+               <button className="pf-login-btn">login</button>
+               <button className="pf-login-btn">signup</button>
+            </div>
+         </div>
+      </header>
+   );
 }
 
 export default Header;
